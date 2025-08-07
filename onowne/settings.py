e2400be1d4501f44885c37b0f3465d524e15e19c
maulik8200 <mmonpara368@gmail.com>
2025-08-07 11:24:13 +0530

    done

--- onowne/settings.py
+++ onowne/settings.py
@@ -65,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'myapp.context_processors.cart_item_count',  # Custom context processor
             ],
         },
     },
